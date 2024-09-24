824ef0cf8cf26bb741b02149047bdf386eec5415
LALA <126696867+lalala-lalal@users.noreply.github.com>
2024-09-24 19:36:57 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<!-- <?php 
+
+    $dbHost = '127.0.0.1';
+    $dbUsername = 'root';
+    $dbPassword = 'Lailinha1@';
+    $dbName = 'spotify';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+    if($conexao->connect_errno)
+    {
+        die("falha".$mysqli->error);
+    }else{
+        echo "Conecção efetuada com sucesso";
+    }
+
+?> -->
